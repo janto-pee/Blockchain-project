aa8dce635a11b40fa392a5e2de46cac2a5955e29
Ayobami Adejumo <ayobami_adejumo@yahoo.com>
2022-07-27 21:28:27 +0100

    sellym web3.0 initial commit

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -4,13 +4,13 @@ import map from '../Assets/map2.jpg';
 const Services = () => {
   
   return (
-    <div className='py-20'>
+    <div className='py-12 md:py-20'>
       <div className="flex flex-col md:flex-row text-gray-100 gap-6 my-6 text-center md:text-left">
         <div className="md:basis-1/2">
           <h4 className="text-2xl font-semibold text-white">Our top users are all over the world with wide coverage</h4>
           <p className='hidden md:flex text-gray-400 mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis illum optio nisi repellendus nihil ab velit. Corporis unde alias reiciendis facere fugiat, vitae distinctio?</p>
           <p className='text-gray-400 md:hidden mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis illum optio nisi repellendus?</p>
-          <div className="my-6 flex md:flex-row gap-4">
+          <div className="hidden my-6 lg:flex md:flex-row gap-4">
             {
               [...Array(3)].map((item, i) => (
                 <div key={i} className=''>
--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,34 +1,33 @@
 import React, {useContext, useState} from 'react'
 import Button from './Button'
-import hero from '../Assets/hero3.png'
+import toke from '../Assets/hero3.png'
 import { SiBlockchaindotcom } from 'react-icons/si'
 import { BiHelpCircle } from 'react-icons/bi'
 import Input from './Input'
 import { TransactionContext } from '../Context/TransactionContext'
 import { shortener } from '../utils/AddressShortener'
-import Loader from './Loader'
 
 
 const Welcome = () => {
   
-  const {connectWallet, currentAccount, isLoading, formData, sendTransaction,handleSend, handleForm} = useContext(TransactionContext); 
+  const {connectWallet, currentAccount, formData, sendTransaction,handleSend, handleForm} = useContext(TransactionContext); 
   
   return (
     <div className='overflow-hidden'>
-      <div className="flex flex-col md:flex-row items-center gap-6 py-10 md:py-20 justify-between ">
+      <div className="flex flex-col md:flex-row items-center gap-6 py-12 md:py-20 justify-between">
         <div className='w-full md:basis-1/2 lg:basis-7/12 flex justify-between items-center text-center md:text-start relative'>
-          <div className='md:basis-6/6'>
+          <div className='md:basis-full lg:basis-4/6 '>
             <small className='text-amber-200'>TRUSTED CRYPTOS</small>
-            <h1 className='hidden md:block text-5xl font-semibold text-white md:mt-4 mt-2 '>Buy and Sell Cryptos</h1>
-            <h1 className='text-3xl md:font-semibold md:text-4xl md:hidden md:font-semibold text-white mt-2 '>Buy and Sell Cryptocurrency</h1>
-            <p className="text-gray-500 mt-3 mb-6 md:max-w-sm">Lorem ipsum dolor sit amet.</p>
+            <h1 className='hidden md:block text-5xl font-semibold text-white mt-2 '>Buy and Sell Cryptos</h1>
+            <h1 className='text-3xl md:hidden font-semibold text-white mt-2 '>Buy and Sell Cryptocurrency</h1>
+            <p className="text-gray-500 mt-3 mb-6 md:max-w-sm">Lorem ipsum dolor sit amet</p>
             {
               !currentAccount && (<Button buttonClass={'hidden md:flex px-6 py-3 bg-secondaryColor font-semibold'} buttonName={'Connect Wallet'} btnClick={connectWallet} />)
             }
           </div>
 
-          <div>
-            <img src={hero} alt="" className='hidden lg:flex absolute top-[-5rem] right-[-3.8rem] w-4/6' />
+          <div >
+            <img src={toke} alt="" className='hidden lg:flex absolute top-[-5rem] right-[-3.8rem] w-4/6' />
           </div>
         </div>
         <div className='w-full md:basis-1/2 lg:basis-4/12 flex flex-col'>
@@ -46,9 +45,7 @@ const Welcome = () => {
             <Input inputType={'text'} inputName='addressTo' inputChange={handleForm} inputPlaceholder={'Address To'}  />
             <Input inputType={'number'} inputName='amount' inputChange={handleForm} inputPlaceholder={'Amount'} />
             <Input inputType={'text'} inputName='message' inputChange={handleForm} inputPlaceholder={'Message'} />
-            {
-              isLoading ? Loader : (<Button buttonClass={'px-6 py-3 bg-primaryColor text-white mb-2 font-semibold w-full hover:bg-sky-300'} buttonName={'Send Now'} btnClick={handleSend} />)
-            }
+            <Button buttonClass={'px-6 py-3 bg-primaryColor text-white mb-2 font-semibold w-full hover:bg-sky-300'} buttonName={'Send Now'} btnClick={handleSend} />
           </div>
         </div>
       </div>
